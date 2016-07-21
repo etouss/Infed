58b843a1c5d9c58f1e9a8f4bb95a623836e76abe
Etienne Toussaint <etouss@Etiennes-MacBook-Air.local>
2016-07-21 11:13:57 +0100

    :)

---
+++ Hook/test26.sql
@@ -0,0 +1,28 @@
+EXPLAIN ANALYSE SELECT o_orderkey FROM orders
+WHERE
+NOT EXISTS(SELECT * FROM lineitem,(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL) as part_view,((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2)) as nation_view
+                WHERE l_orderkey = o_orderkey
+                AND l_partkey = p_partkey
+                AND l_suppkey = s_suppkey)
+AND NOT EXISTS (SELECT * FROM lineitem,(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL) as part_view,((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2)) as nation_view
+                WHERE l_orderkey = o_orderkey
+                AND l_partkey = 2
+                AND l_suppkey = s_suppkey
+                AND EXISTS(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL))
+AND NOT EXISTS (SELECT * FROM lineitem,(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL) as part_view,((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2)) as nation_view
+                WHERE l_orderkey = o_orderkey
+                AND l_partkey = 2
+                AND l_suppkey = 2
+                AND EXISTS(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL)
+                AND EXISTS((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2)))
+AND NOT EXISTS(SELECT * FROM lineitem,(SELECT * FROM part WHERE p_name LIKE '%floral%' OR p_name IS NULL) as part_view,((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2))as nation_view
+                WHERE l_orderkey = o_orderkey
+                AND l_partkey = p_partkey
+                AND l_suppkey = 2
+                AND EXISTS((Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = n_nationkey)
+                                UNION ALL (Select * FROM supplier,(SELECT * FROM nation WHERE n_name = 'FRANCE') as n WHERE s_nationkey = 2)))
